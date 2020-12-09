a077c2e65173bf31a01b8b5b6dcad95ddbf0b5a2
Mohayemin <mohayeminul.islam@gmail.com>
2020-12-09 18:50:34 +0600

    fix analysis script

--- src/analysis-scripts/parallel_refactoring_frequency.sql
+++ src/analysis-scripts/parallel_refactoring_frequency.sql
@@ -1,20 +1,34 @@
-select @total_refactorings := count(*) from refactoring;
+set @total_refactorings := (
+    select count(*)
+    from refactoring
+);
 
-select @refactorings_with_parallel_refactoring:= count(distinct pr.refactoring_id)
-from (
-         select distinct refactoring1_id refactoring_id from parallel_refactoring
-         union
-         select distinct refactoring2_id refactoring_id from parallel_refactoring
-     ) pr;
+set @refactorings_with_parallel_refactoring := (
+    select count(distinct pr.refactoring_id)
+    from (
+             select distinct refactoring1_id refactoring_id
+             from parallel_refactoring
+             union
+             select distinct refactoring2_id refactoring_id
+             from parallel_refactoring
+         ) pr
+);
 
-select @total_refactoring_commits := count(*) from refactoring_commit;
+set @total_refactoring_commits := (
+    select count(*)
+    from refactoring_commit
+);
 
-select @refactoring_commits_with_parallel_refactoring_commits := count(distinct pr.commit_id)
-from (
-         select distinct refactoring1_commit_id commit_id from parallel_refactoring
-         union
-         select distinct refactoring2_commit_id commit_id from parallel_refactoring
-     ) pr;
+set @refactoring_commits_with_parallel_refactoring_commits := (
+    select count(distinct pr.commit_id)
+    from (
+             select distinct refactoring1_commit_id commit_id
+             from parallel_refactoring
+             union
+             select distinct refactoring2_commit_id commit_id
+             from parallel_refactoring
+         ) pr
+);
 
 select @total_refactorings,
        @refactorings_with_parallel_refactoring,
