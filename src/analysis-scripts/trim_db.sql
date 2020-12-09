3e7205b859549da71e7aca159c9d2942ca6e056b
Mohayemin <mohayeminul.islam@gmail.com>
2020-12-10 04:00:38 +0600

    remove bad project

--- src/analysis-scripts/trim_db.sql
+++ src/analysis-scripts/trim_db.sql
@@ -15,6 +15,9 @@ delete from project where id > @project_count;
 -- The following projects are no longer accessible at the given URL
 delete from project where id in (61,127,166,205,360,438);
 
+-- this project was making trouble in many ways
+delete from project where id in (25);
+
 
 delete from conflicting_java_file where project_id not in (select id from project);
 delete from conflicting_region where project_id not in (select id from project);
